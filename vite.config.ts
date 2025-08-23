d655a995d7e9a8014d1659b1eafbc86fb78b3ddc
Federico Zizi <federicoziziofficial@gmail.com>
2025-08-23 12:35:06 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "127.0.0.1", // forza IPv4 invece di IPv6
+    port: 5173         // tieni la porta di default
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
